e4bcf129998dc63fdd38a38209c08701f04dba8e
murgol75 <murgol@yahooo.fr>
2023-11-30 14:46:40 +0100

    manage user géré en async + observable

--- src/app/features/auth/manage-users/manage-users.component.ts
+++ src/app/features/auth/manage-users/manage-users.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { User } from 'src/app/shared/models/user';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AuthService } from 'src/app/shared/services/auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-manage-users',
@@ -10,38 +11,22 @@ import { AuthService } from 'src/app/shared/services/auth.service';
   styleUrls: ['./manage-users.component.scss']
 })
 
-  
+
 export class ManageUsersComponent {
 
-  
-  
-  
-  errorUser : string = '';
-  userUrl : string ="http://localhost:3000/users";
-  users : User[] = [];
-  
-  constructor(private _httpClient : HttpClient,
-    private _auth : AuthService) {}
-  
+
+
+
+  errorUser: string = '';
+  userUrl: string = "http://localhost:3000/users";
+  users: User[] = [];
+  $users: Observable<User[]>;
+
+  constructor(private _httpClient: HttpClient,
+    private _auth: AuthService) { }
+
   ngOnInit(): void {
-    this._auth.$users.subscribe({next : (value) => {this.users = value}})
+    this.$users = this._auth.$users
     this._auth.getAll();
-}
-
-
-// getAll() : void {
-//   this.errorUser = '';
-
-//   this._httpClient.get<User[]>(this.userUrl).subscribe({
-//     next : (value) => { this.users = value },
-//     error : (error) => { 
-//       console.log(error);         
-//       if(error.status === 401) {
-//         this.errorUser = 'Vous devez être connecté.e pour voir les utilisateurs';
-//       }
-//      }
-    
-//   })
-// }
-
+  }
 }
